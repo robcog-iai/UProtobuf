76fddcddaf4cf62365bd854ceec20cb8f64490bd
Xiaojun <xiaojun.real@outlook.com>
2020-08-19 12:22:50 +0200

    Add google protobuf 3.12.3

---
+++ Source/Protobuf/UProtobuf.Build.cs
@@ -0,0 +1,69 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.IO;
+
+public class UProtobuf : ModuleRules
+{
+    private string LibProtoPathR
+    {
+        get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "../ThirdParty/lib/Release/libprotobuf.lib")); }
+    }
+    private string HeaderPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "../ThirdParty/include")); }
+    }
+
+    public UProtobuf(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+                HeaderPath
+            }
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+                // ... add other private include paths required here ...
+                HeaderPath
+            }
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+
+        if (Target.Platform == UnrealTargetPlatform.Win64)
+        {
+            PublicAdditionalLibraries.Add(LibProtoPathR);
+        }
+
+        bEnableUndefinedIdentifierWarnings = false;
+        bEnableShadowVariableWarnings = false;
+    }
+}
